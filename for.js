6dcc1a1a9271a3edde79dce80c65e2bedaabdb25
Rianzinhu <rianrochaaraujo1@gmail.com>
2025-08-07 20:09:14 -0300

    Aula 02-pw3

---
+++ for.js
@@ -0,0 +1,19 @@
+function validarFaculdade()
+{
+var verificar = prompt("Digite uma opção");
+switch (verificar)
+{
+case "1":
+alert("Essa faculdade ainda não está no nível da FIAP");
+break;
+case "2":
+alert("Essa faculdade tem que evoluir muito para chegar no nível da FIAP");
+break;
+case "3":
+alert("Essa é a melhor faculdade, essa é a FIAP");
+break;
+default:
+alert("A faculdade digitada não está cadastrada.");
+break;
+}
+}
---
+++ funcoes.js
@@ -0,0 +1,5 @@
+function mostrarCincoAlertscomFor(){
+    var numero;
+    for(numero = 1; numero <=5; numero ++)
+        alert (numero);
+}
